d67bb361a427af7d21c38c29898e03f15a7d3e35
PauloMisaki <ph.sorata@gmail.com>
2022-07-31 23:33:56 -0300

    fix: yellow letter issue

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,24 +1,27 @@
 import React, { useContext, useEffect } from "react";
-import { AppContext } from '../App';
+import { AppContext } from "../App";
 
 function Letter({ letterPos, attemptVal }) {
   const { board, correctWord, currentAttempt, setDisabledLetters } = useContext(AppContext);
   const letter = board[attemptVal][letterPos];
-
   const correct = correctWord.toUpperCase()[letterPos] === letter;
-  const almost = !correct && letter !== '' && correctWord.includes(letter);
-
-  const letterState = currentAttempt.attempt > attemptVal && (correct ? 'correct' : almost ? 'almost' : 'error');
+  const almost =
+    !correct && letter !== "" && correctWord.toUpperCase().includes(letter);
+  const letterState =
+    currentAttempt.attempt > attemptVal &&
+    (correct ? "correct" : almost ? "almost" : "error");
 
   useEffect(() => {
-    if (letter !== '' && !correct && !almost) {
+    if (letter !== "" && !correct && !almost) {
+      console.log(letter);
       setDisabledLetters((prev) => [...prev, letter]);
     }
   }, [currentAttempt.attempt]);
-
-  return(
-    <div className="letter" id={letterState}>{letter}</div>
-  )
+  return (
+    <div className="letter" id={letterState}>
+      {letter}
+    </div>
+  );
 }
 
 export default Letter;
